f1b20c555fddbf6fe0167ffd85bfd17e1d0e9bd1
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-04 12:53:17 +0200

    snack 3

--- index.php
+++ index.php
@@ -22,6 +22,30 @@ $posts = [
     ]
   ],
 ];
+
+$db = [
+  'teachers' => [
+    [
+      'name' => 'Michele',
+      'lastname' => 'Papagni'
+    ],
+    [
+      'name' => 'Fabio',
+      'lastname' => 'Forghieri'
+    ]
+  ],
+  'pm' => [
+    [
+      'name' => 'Roberto',
+      'lastname' => 'Marazzini'
+    ],
+    [
+      'name' => 'Federico',
+      'lastname' => 'Pellegrini'
+    ]
+  ]
+];
+
 ?>
 
 
@@ -43,25 +67,25 @@ $posts = [
     <h1 class="text-center mb-5">Elenco dei Post</h1>
 
     <?php foreach ($posts as $date => $postList) { ?>
-    <div class="card mb-4">
-      <div class="card-header bg-primary text-white">
-        Data: <?= $date ?>
+      <div class="card mb-4">
+        <div class="card-header bg-primary text-white">
+          Data: <?= $date ?>
+        </div>
+        <div class="card-body">
+          <?php foreach ($postList as $post) { ?>
+            <h5 class="card-title"><?= $post['title'] ?></h5>
+            <h6 class="card-subtitle mb-2 text-muted">Autore: <?= $post['author'] ?></h6>
+            <p class="card-text"><?= $post['text'] ?></p>
+            <hr>
+          <?php } ?>
+        </div>
       </div>
-      <div class="card-body">
-        <?php foreach ($postList as $post) { ?>
-        <h5 class="card-title"><?= $post['title'] ?></h5>
-        <h6 class="card-subtitle mb-2 text-muted">Autore: <?= $post['author'] ?></h6>
-        <p class="card-text"><?= $post['text'] ?></p>
-        <hr>
-        <?php } ?>
-      </div>
-    </div>
     <?php } ?>
 
     <h1 class="text-danger">Snack 2</h1>
     <h1 class="text-center mb-5">Verifica Dati</h1>
 
-    <form action="" method="GET">
+    <form action="index.php" method="GET">
       <div class="form-group">
         <label for="name">Nome</label>
         <input type="text" class="form-control" id="name" name="name" required>
@@ -93,6 +117,20 @@ $posts = [
       }
       ?>
     </div>
+
+    <h1 class="text-danger">Snack 3</h1>
+    <?php
+    foreach ($db as $role => $people) {
+      $class = ($role === 'teachers') ? 'bg-light' : 'bg-success text-white';
+      foreach ($people as $person) { ?>
+        <div class="<?= $class ?>">
+          <h4><?= $role ?>:</h4>
+          <p><strong>Nome:</strong> <?= $person['name'] ?></p>
+          <p><strong>Cognome:</strong> <?= $person['lastname'] ?></p>
+        </div>
+    <?php }
+    }
+    ?>
 
   </div>
 
